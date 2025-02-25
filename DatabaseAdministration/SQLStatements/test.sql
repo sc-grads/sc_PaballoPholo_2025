d9370f1b430ba37dcab83a9655dff540ef5ac040
Paballopj27 <paballo.pholo@sambeconsulting.com>
2025-02-25 15:14:10 +0200

    Added simple test SQL script

---
+++ DatabaseAdministration/SQLStatements/test.sql
@@ -0,0 +1,23 @@
+
+-- Create a test database
+CREATE DATABASE TestDB;
+GO
+
+-- Switch to the test database
+USE TestDB;
+GO
+
+-- Create a simple table
+CREATE TABLE TestTable (
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    name VARCHAR(50) NOT NULL
+);
+GO
+
+-- Insert some test data
+INSERT INTO TestTable (name) VALUES ('Alice'), ('Bob');
+GO
+
+-- Retrieve the test data
+SELECT * FROM TestTable;
+GO
