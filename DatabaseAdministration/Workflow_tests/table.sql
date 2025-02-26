3cfb2296efe625e97a34760e715874bdd653901d
Paballopj27 <paballo.pholo@sambeconsulting.com>
2025-02-26 13:46:42 +0200

    Created a folder to test workflows on and added a testing script

---
+++ DatabaseAdministration/Workflow_tests/table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(50),
+    email VARCHAR(100) UNIQUE,
+    age INT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
