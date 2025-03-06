f86cd25df82bb800053c31e1c640a19463e46ac3
Paballopj27 <paballo.pholo@sambeconsulting.com>
2025-03-06 13:57:06 +0200

    Add basic functions example in Python

---
+++ Python/Functions.py
@@ -0,0 +1,11 @@
+x = 5
+print ('hello')
+
+def print_lyrics():
+    print("I'm a lamberjack and i'm okay")
+    print("I sleep all night and I work all day")
+    
+print('yo')
+print_lyrics()
+x = x + 2
+print(x)
