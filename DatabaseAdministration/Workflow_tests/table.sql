927539f693ecd5f58903672e893d576ad320715d
Paballo-Pholo <paballo.pholo@sambeconsulting.com>
2025-03-20 11:40:30 +0200

    Delete DatabaseAdministration/Workflow_tests/table.sql

--- DatabaseAdministration/Workflow_tests/table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(50),
-    email VARCHAR(100) UNIQUE,
-    age INT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
