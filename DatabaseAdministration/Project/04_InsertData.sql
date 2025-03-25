74639dbe66d4c5bcb84ec5fdfe277d53f5aeb5ad
Paballo-Pholo <paballo.pholo@sambeconsulting.com>
2025-03-25 08:48:43 +0200

    Delete DatabaseAdministration/Project/04_InsertData.sql

--- DatabaseAdministration/Project/04_InsertData.sql
+++
@@ -1,6 +0,0 @@
-USE AutoTest;
-GO
-
-EXEC InsertUser @Name='Paballo', @Surname='Pholo', @Email='paballo@example.com';
-EXEC InsertUser @Name='Lerato', @Surname='Molefe', @Email='lerato@example.com';
-GO
