1445f516a1672c0988d2409a88ee7f12da040e32
Paballopj27 <paballo.pholo@sambeconsulting.com>
2025-04-02 15:28:31 +0200

    Adding the updates

--- Linux/Linux_Commands.sh
+++ Linux/Linux_Commands.sh
@@ -54,5 +54,32 @@ head file           #Output the beginning (or top) portion of file.
 tail file Output    #the ending (or bottom) portion of file
 tail -f file        #Follow the file - Displays data as it is being written to the file.
 
-#
+#Removing files
 # -------------------
+rm file     #Remove file.
+rm -r dir   #Remove the directory and its contents recursively.
+rm -f file  #Force removal and never prompt for confirmation
+
+#Copying files
+# -------------------
+cp source_file destination_file #Copy source_file to destination_file.
+
+#cp otions
+cp -i #Run in interactive mode
+cp -r #Copy src_directory recursively to destination. 
+
+#Moving and Renaming Files
+# -------------------
+ mv # Move or rename files and directories.
+
+ #SortingData
+ sort file      #Sort text in file
+ sort -r file   #Sort in reverse order
+ sort -u file   # Sort unique
+ sort -k F file #Sort by key.  F is the field number
+
+#Aliases
+# -------------------
+alias [name[=value]] #List or create aliases. Use name=value to create a new alias
+unalias name #Remove the “name” alias
+unalias -a #Remove all aliases
