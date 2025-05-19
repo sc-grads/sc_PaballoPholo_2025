72621b147af2635d48f919d01972b170ad1cc390
Paballopj27 <paballo.pholo@sambeconsulting.com>
2025-05-19 15:51:19 +0200

    Added the first queries script and the first practice activity

---
+++ DatabaseAdministration/T-SQL/First_Queries.sql
@@ -0,0 +1,8 @@
+SELECT 1+1 AS Result -- Select is equalivalent to Print in other programming languages
+GO
+SELECT 1*1 AS Result -- As is an alias, Naming our column 
+GO
+select 1/0 As Result -- Result is the name of our column
+
+SELECT 1/1 As Result -- 1+1 and 1*1 are mathematical expressions, what we want to be shown, Select only shows the results
+GO
