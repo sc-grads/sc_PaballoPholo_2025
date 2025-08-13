7d02e0cbaa5a8a11c93e44ab6bda7341efdb68ba
Paballopj27 <paballo.pholo@sambeconsulting.com>
2025-08-13 15:28:14 +0200

    Added my notebooks for python for Data Science

--- PythonForDataScience/PythonCrashCourse/Part2.py
+++ PythonForDataScience/PythonCrashCourse/Part2.py
@@ -5,3 +5,16 @@ print(d['key1'])  # Output: value1
 
 #A list as a key
 d1 = {'k1': [1, 2, 3]}
+
+#Tuples
+t = (1, 2, 3)
+print(t[0])  # Output: 1
+
+#Tuples are immutable, we use them if we don't want the data to change
+
+#Sets
+s = {1, 2, 3, 4, 5} 
+#allows only unique elements - that is no duplicates
+
+s.add(6)  # Adding an element
+print(s)  # Output: {1, 2, 3, 4, 5, 6}
